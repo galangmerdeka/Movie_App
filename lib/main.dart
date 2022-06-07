da78af6b97d9452d2a35e13685b4730c10d2d116
galangmerdeka <galang186@gmail.com>
2022-06-07 19:07:03 +0700

    update cert in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/app_widget.dart';
 
 void main() async {
+  HttpOverrides.global = MyHttpOverrides(); //Just For Development only
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const AppWidget());
+}
+
+//Just For Development only
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
